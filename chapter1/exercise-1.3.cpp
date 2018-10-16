bff927624f56e66d0fe586a634b32fb5e60718c5
Heatwave <stan_yxr@163.com>
2018-10-16 22:52:21 +0800

    exercises 3-5

---
+++ chapter1/exercise-1.3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main3()
+{
+	std::cout << "Hello, World" << std::endl;
+	system("pause");
+	return 0;
+}
--- chapter1/exercise-1.4.cpp
+++ chapter1/exercise-1.4.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-int mainio()
+int main4()
 {
 	std::cout << "Enter two numbers:" << std::endl;
 	int v1 = 0, v2 = 0;
 	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2
-		<< " is " << v1 + v2 << std::endl;
+	std::cout << "The product of " << v1 << " and " << v2
+		<< " is " << v1 * v2 << std::endl;
 
 	system("pause");
 	return 0;
